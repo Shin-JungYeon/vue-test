381c969b7756a1f7688ec0775d9941185c04f83c
신정연 <ssjy11@gmail.com>
2022-10-14 11:31:29 +0900

    vue-router 버전 다운그레이드 및 <router-link> 추가

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <!-- 최상위 컴포넌트 -->
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<!--<script>-->
+<!--import HelloWorld from './components/HelloWorld.vue'-->
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<!--export default {-->
+<!--  name: 'App',-->
+<!--  components: {-->
+<!--    HelloWorld-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
 
 <style>
 #app {
@@ -24,6 +27,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+// Vue 2.x 버전에서는 vue-router 3.x 버전 사용
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
